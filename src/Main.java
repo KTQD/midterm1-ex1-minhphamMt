7339821dd458b52288c6bc0faf4f8bd5f443d0f7
MinhPham28 <142344042+minhphamMt@users.noreply.github.com>
2024-03-14 07:56:45 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        MP3 mp3 = new MP3("nhacmp3", "hay choi nhac", "123", 55.99d, 3500);
+        MP3 mp3 = new MP3("nhacmp3", "hay choi nhac", "123", 55.99f, 3500);
         mp3.showInfo();
         System.out.println(" ");
-        Book book = new Book("hoc java nang cao", "hoc ngon ngu java", "123", 20.55d, "pham xuan lam", 600, "thuc hanh");
+        Book book = new Book("hoc java nang cao", "hoc ngon ngu java", "123", 20.55f, "pham xuan lam", 600, "thuc hanh");
         book.showInfo();
     }
 }
